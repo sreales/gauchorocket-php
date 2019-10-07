3288660e8fa663deb06236a1e7d3b57cc252261d
sreales <sebastian.reales@gmail.com>
2019-10-06 23:29:21 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <bod>
+        <header>
+        </header>
+        <nav></nav>
+        <article>
+            
+        </article>
+    </bod>
+</html>
